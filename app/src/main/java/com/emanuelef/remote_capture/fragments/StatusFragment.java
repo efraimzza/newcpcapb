471ff1a2d76696c346c89a034d8e56a605c30cf5
efraimzz <85498030+efraimzz@users.noreply.github.com>
2025-05-26 08:41:22 +0300

    Update StatusFragment.java

--- app/src/main/java/com/emanuelef/remote_capture/fragments/StatusFragment.java
+++ app/src/main/java/com/emanuelef/remote_capture/fragments/StatusFragment.java
@@ -447,7 +447,7 @@ public class StatusFragment extends Fragment implements AppStateListener, MenuPr
     }
     void mactivatepcapmdm() {
 		try {
-            DevicePolicyManager dpm=((DevicePolicyManager)mActivity.getSystemService(device_policy));
+            DevicePolicyManager dpm=(DevicePolicyManager)mActivity.getSystemService(device_policy);
             dpm.addUserRestriction(compName, UserManager.DISALLOW_DEBUGGING_FEATURES);
             //dpm.setPackagesSuspended(compName,new String[]{getPackageName()},true);
             dpm.addUserRestriction(compName, UserManager.DISALLOW_FACTORY_RESET);
@@ -491,7 +491,7 @@ public class StatusFragment extends Fragment implements AppStateListener, MenuPr
 	}
 	void mremovepcapmdm() {
 		try {
-            DevicePolicyManager dpm=((DevicePolicyManager)mActivity.getSystemService(device_policy));
+            DevicePolicyManager dpm=(DevicePolicyManager)mActivity.getSystemService(device_policy);
 
             dpm.clearUserRestriction(compName, UserManager.DISALLOW_DEBUGGING_FEATURES);
             dpm.clearUserRestriction(compName, UserManager.DISALLOW_UNINSTALL_APPS);
@@ -509,20 +509,20 @@ public class StatusFragment extends Fragment implements AppStateListener, MenuPr
                 Bundle bundle = new Bundle();
                 bundle = null;
                 String str = "com.google.android.gms";
-                ((DevicePolicyManager)mActivity.getSystemService(device_policy)).setApplicationRestrictions(compName, str, bundle);
+                (DevicePolicyManager)mActivity.getSystemService(device_policy).setApplicationRestrictions(compName, str, bundle);
                 Intent intent = new Intent("com.google.android.gms.auth.FRP_CONFIG_CHANGED");
                 intent.setPackage(str);
                 intent.addFlags(268435456);
                 mcon.sendBroadcast(intent);
-                ((DevicePolicyManager)mActivity.getSystemService(device_policy)).clearDeviceOwnerApp(getPackageName());
+                (DevicePolicyManager)mActivity.getSystemService(device_policy).clearDeviceOwnerApp(getPackageName());
 
                 Toast.makeText(mcon, "removed", Toast.LENGTH_SHORT).show();
             } catch (Exception e) {
                 Toast.makeText(mcon, "" + e, Toast.LENGTH_SHORT).show();
             }
             try {
-                if (((DevicePolicyManager)mActivity.getSystemService(device_policy)).isAdminActive(compName)) {
-                    ((DevicePolicyManager)mActivity.getSystemService(device_policy)).removeActiveAdmin(compName);
+                if ((DevicePolicyManager)mActivity.getSystemService(device_policy).isAdminActive(compName)) {
+                    (DevicePolicyManager)mActivity.getSystemService(device_policy).removeActiveAdmin(compName);
 
                     Toast.makeText(mcon, "removed active admin", Toast.LENGTH_SHORT).show();
                 }
