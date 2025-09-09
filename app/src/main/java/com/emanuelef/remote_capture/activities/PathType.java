f4a54d0c374ffe810cec142c61a67b866c8cb632
efraimzz <hefraimzz@gmail.com>
2025-09-09 20:47:51 +0300

    .

--- app/src/main/java/com/emanuelef/remote_capture/activities/PathType.java
+++ app/src/main/java/com/emanuelef/remote_capture/activities/PathType.java
@@ -3,10 +3,10 @@ package com.emanuelef.remote_capture.activities;
 public enum PathType {
     MULTIMEDIA("מולטימדיה"),
     MULTIMEDIA_ACCESSIBILITY("מולטימדיה ונגישות"),
-    EVERYTHING("הכל");
-    MMAPS("מפות");
-    WAZE("וויז");
-    MAIL("מייל");
+    EVERYTHING("הכל"),
+    MAPS("מפות"),
+    WAZE("וויז"),
+    MAIL("מייל"),
     NAVIGATIONMUSICAPPS("ניווט ומוזיקה");
 
     private final String description;
